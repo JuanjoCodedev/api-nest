9723a449b6f7a2c887bc96e9176fa97eb9886d29
abc <abc@example.com>
2023-09-16 11:45:20 -0500

    first commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,9 @@ import { UserModule } from './client/user.module';
 import { conexionPostgreSQL } from './database/conexion';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
-import { ArticlesModule } from './articles/articles.module';
-import { CartModule } from './cart/cart.module';
 
 @Module({
-  imports: [UserModule, conexionPostgreSQL, AuthModule, ConfigModule.forRoot({ envFilePath: '.development.env', isGlobal: true }), ArticlesModule, CartModule],
+  imports: [UserModule, conexionPostgreSQL, AuthModule, ConfigModule.forRoot({ envFilePath: '.development.env', isGlobal: true })],
   controllers: [],
   providers: [],
 })
--- src/client/user.controller.ts
+++ src/client/user.controller.ts
@@ -16,6 +16,7 @@ interface getUserWithRoles extends Request {
 export class UserController {
   constructor(private userService: UserService) {}
 
+  @Auth(Role.USER)
   @Post('/crear-cuenta') //ruta: http://localhost:3000/user/registro-usuario
   async createUser(@Body() user: userDTO) {
     return await this.userService.createUser(user);
@@ -29,11 +30,13 @@ export class UserController {
     return userProfile;
   }
 
+  @Auth(Role.USER)
   @Patch('/modificar-cuenta/:uid') //ruta: http://localhost:3000/user/actualizar-usuario/:uid
   async updateUser(@Param('uid') uid: number, @Body() UpdatedPersonDTO: userUpdateDTO): Promise<User> {
     return await this.userService.updateUser(uid, UpdatedPersonDTO);
   }
 
+  @Auth(Role.USER)
   @Delete('/eliminar-cuenta/:uid') //ruta: http://localhost:3000/user/eliminar-usuario/:uid
   async deleteUser(@Param('uid') uid: number) {
     return await this.userService.deleteUser(uid);
